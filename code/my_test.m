438d7a096c39d37620fd3368fe3f16bb510f360c
Asalea <asa893473663@gmail.com>
2019-05-27 13:58:08 +0800

    Update my_test.m

--- code/my_test.m
+++ code/my_test.m
@@ -2,18 +2,33 @@
 %Character Recongnition
 
 clc; clear all; close all;
-imgInput = imread('../asset/image/test4.jpg');
+imgInput = imread('../asset/image/test.jpg');
 %imgInput = imread('../asset/image/1_part.png');
 imgInput = rgb2gray(imgInput);
 
-
-%imgInput = imresize(imgInput, 0.5);
 figure, imshow(imgInput);
 
-ocrResults = ocr(imgInput, 'CharacterSet', '0123456789+-*/=');
+BW = imbinarize(imgInput);
+BW1 = imdilate(BW, strel('disk', 6));
+s = regionprops(BW1, 'BoundingBox');
+bboxes = vertcat(s(:).BoundingBox);
+
+[~,ord] = sort(bboxes(:,2));
+bboxes = bboxes(ord, :);
+
+BW = imdilate(BW, strel('disk', 1));
+
+ocrResults = ocr(BW, bboxes, 'CharacterSet', '012345678+-*/=', 'TextLayout', 'Word');
+words = {ocrResults(:).Text};
+words = deblank(words)
+
+%imgInput = imresize(imgInput, 0.5);
+%figure, imshow(imgInput);
+
+%ocrResults = ocr(imgInput, 'CharacterSet', '0123456789+-*/=');
 %Iocr = insertObjectAnnotation(imgInput, 'rectangle', ocrResults.WordBoundingBoxes, ocrResults.WordConfidences);
 %figure; imshow(Iocr);
-ocrResults.Text
+%ocrResults.Text
 
 
 % imgOutput = my_calculator(imgInput);
