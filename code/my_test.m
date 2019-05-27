500c49fc2269e92245b20cd0e019830730481ed2
Asalea <asa893473663@gmail.com>
2019-05-27 13:48:49 +0800

    Update my_test.m

--- code/my_test.m
+++ code/my_test.m
@@ -1,16 +1,20 @@
 %DIP19 Assignment 3
 %Character Recongnition
 
-clc; clear all;
-imgInput = imread('../asset/image/test1.jpg');
+clc; clear all; close all;
+imgInput = imread('../asset/image/test4.jpg');
+%imgInput = imread('../asset/image/1_part.png');
 imgInput = rgb2gray(imgInput);
 
 
-BW = imbinarize(imgInput);
+%imgInput = imresize(imgInput, 0.5);
+figure, imshow(imgInput);
 
+ocrResults = ocr(imgInput, 'CharacterSet', '0123456789+-*/=');
+%Iocr = insertObjectAnnotation(imgInput, 'rectangle', ocrResults.WordBoundingBoxes, ocrResults.WordConfidences);
+%figure; imshow(Iocr);
+ocrResults.Text
 
-results=ocr(imgInput, 'TextLayout', 'Block');
-results.Text
 
 % imgOutput = my_calculator(imgInput);
 %imgOutput = imgInput;
