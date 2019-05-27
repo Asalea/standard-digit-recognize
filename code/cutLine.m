bdc8adf05359055469261eb5d1dbc84bff6697aa
Asalea <asa893473663@gmail.com>
2019-05-27 22:14:02 +0800

    将一行中字符框出并保存本地

--- code/cutLine.m
+++ code/cutLine.m
@@ -1,6 +1,15 @@
 function output=cutLine(imgLine)
 [L, num] = bwlabel(imgLine);
 [m, n] = size(imgLine);
+
+%clear dir
+relative = '../asset/image/cut/part/';
+if exist(relative, 'dir')
+	rmdir(relative, 's');
+end
+mkdir(relative);
+
+nameNum = 0;
 for i=1:num-2
 	[r,c]=find(L==i);
 
@@ -15,4 +24,11 @@ for i=1:num-2
 	imgCharacter = imcrop(imgLine, [cmin-10, rmin-10, width+20, height+20]);
     %figure, imshow(imgLine);
 	figure, imshow(imgCharacter);
+
+
+	%write one character to bmp file.
+	address = strcat(relative, num2str(nameNum));
+	address = strcat(address, '.bmp');
+	imwrite(imgCharacter, address);
+	nameNum = nameNum+1;
 end
